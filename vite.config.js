3a2c9f5681bee143779811ae6d2c8fb96d75f51c
Patrizio Milione <patriziomilione@gmail.com>
2023-04-18 15:27:22 +0200

    daje

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/TaskTitan/',
+  base: '/TaskTitanFE/',
   plugins: [vue()],
   resolve: {
     alias: {
