35a9a1b9f0bed0f01fec05d5c4f53f4e35f334b0
Patrizio Milione <patriziomilione@gmail.com>
2023-04-18 15:10:39 +0200

    first commit again

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
+import VueCryptojs from 'vue-cryptojs'
 
 library.add(fas);
 
@@ -15,4 +16,5 @@ app.use(router)
 
 app
 .component('fa', FontAwesomeIcon)
+.use(VueCryptojs)
 .mount('#app')
